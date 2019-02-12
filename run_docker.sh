a99b8963813cf50846cd36dd1755b24c8a2544f8
VVKot <volodymyr.kot.ua@gmail.com>
2019-02-12 20:31:30 +0200

    feat: Make docker build directory agnostic

--- run_docker.sh
+++ run_docker.sh
@@ -1,2 +1,3 @@
-sudo docker build -t pytorch -f docker/pytorch-no-cuda/Dockerfile . && sudo docker run -it -p 6006:6006 -v ~/git-repos/mlinseconds-general-cpu:/mlinseconds-general-cpu -w /mlinseconds-general-cpu --name pytorch --rm pytorch
-sudo docker run -it -p 6006:6006 -v ~/git-repos/mlinseconds-general-cpu:/mlinseconds-general-cpu -w /mlinseconds-general-cpu --name pytorch --rm pytorch
+dir=$(pwd)
+sudo docker build -t pytorch -f docker/pytorch-no-cuda/Dockerfile . && sudo docker run -it -p 6006:6006 -v $dir/mlis:/mlinseconds-find-me -w /mlinseconds-find-me --name pytorch --rm pytorch
+sudo docker run -it -p 6006:6006 -v $dir/mlis:/mlinseconds-find-me -w /mlinseconds-find-me --name pytorch --rm pytorch
